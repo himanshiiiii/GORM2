c1ca780190a1ef73f377a4f0cb9d5787efef1a1c
himanshiiiii <himanshi.gupta@tothenew.com>
2018-03-28 23:12:01 +0530

    Question 6

--- grails-app/init/linksharing/BootStrap.groovy
+++ grails-app/init/linksharing/BootStrap.groovy
@@ -9,6 +9,7 @@ class BootStrap {
 
     def init = { servletContext ->
         createUsers()
+        createTopics()
     }
 
     void createUsers() {
@@ -32,6 +33,50 @@ class BootStrap {
                 log.error("error: ${normal.errors.getFieldErrors()}")
             }
 
+        }
+    }
+
+
+
+    void createTopics() {
+        List<User> userCount = User.getAll()
+        userCount.each {
+
+            if (Topic.findAllByCreatedBy(it).size()==0) {
+                //User normal = User.findByUserName("HimanshiGupta")
+
+                Topic topic = new Topic(name: "BigData", createdBy: it, visibility: Visibility.PUBLIC)
+                it.addToTopics(topic)
+
+                Topic topic1 = new Topic(name: "Java", createdBy: it, visibility: Visibility.PRIVATE)
+                it.addToTopics(topic1)
+
+                Topic topic2 = new Topic(name: "nodeJs", createdBy: it, visibility: Visibility.PUBLIC)
+                it.addToTopics(topic2)
+
+                Topic topic3 = new Topic(name: "AngularJs", createdBy: it, visibility: Visibility.PRIVATE)
+                it.addToTopics(topic3)
+
+                Topic topic4 = new Topic(name: "MachineLearning", createdBy: it, visibility: Visibility.PUBLIC)
+                it.addToTopics(topic4)
+
+                topic.validate()
+                log.error("Topic ${topic.errors.getFieldErrors()}")
+                topic.save()
+                topic1.validate()
+                log.error("Topic ${topic1.errors.getFieldErrors()}")
+                topic1.save()
+                topic2.validate()
+                log.error("Topic ${topic2.errors.getFieldErrors()}")
+                topic2.save()
+                topic3.validate()
+                log.error("Topic ${topic3.errors.getFieldErrors()}")
+                topic3.save()
+                topic4.validate()
+                log.error("Topic ${topic4.errors.getFieldErrors()}")
+                topic4.save()
+
+            }
         }
     }
 
