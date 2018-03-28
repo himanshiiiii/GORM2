421556455623f313073fc928f76ad35e50c074f1
himanshiiiii <himanshi.gupta@tothenew.com>
2018-03-28 18:56:19 +0530

    Question1

--- grails-app/init/linksharing/BootStrap.groovy
+++ grails-app/init/linksharing/BootStrap.groovy
@@ -1,14 +1,33 @@
-package linksharing
+package com.ttn.linksharing
+
+//Add createUsers method to Bootstrap to create 1 admin and 1 normal user
+
 
 class BootStrap {
 
-
     def init = { servletContext ->
+        createUsers()
+    }
+    void createUsers(){
+
+        //admin
+        User admin=new User(email: "admin@gmail.com",password: "admin@123",firstName: "admin",lastName: "Linksharing",userName:'adminLinksharing' , photo: 121,admin:true,active:true)
+        admin.validate()
+        log.error "error: ${admin.errors.getFieldError()}"
+        admin.save(flush:true)
+
+
+        //normal
+        User normal=new User(email: "himanshigupta238@gmail.com",password: "admin@123",firstName: "Himanshi",lastName: "Gupta",userName:'HimanshiGupta',photo: 122,admin:false,active:true)
+        normal.validate()
+        log.error "error: ${normal.errors.getFieldError()}"
+        normal.save(flush:true)
+
+
+
+
 
     }
-
-
     def destroy = {
     }
-
 }
