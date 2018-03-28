4fd78e12bc529fc662612991d5b87f2cf9f6477b
himanshiiiii <himanshi.gupta@tothenew.com>
2018-03-28 22:54:13 +0530

    Question4

--- grails-app/init/linksharing/BootStrap.groovy
+++ grails-app/init/linksharing/BootStrap.groovy
@@ -1,5 +1,7 @@
 package com.ttn.linksharing
 
+import constant.DefaultPassword
+
 //Add createUsers method to Bootstrap to create 1 admin and 1 normal user
 
 
@@ -8,37 +10,31 @@ class BootStrap {
     def init = { servletContext ->
         createUsers()
     }
-    void createUsers(){
 
-        //admin
-        User admin=new User(email: "admin@gmail.com",password: "admin@123",firstName: "admin",lastName: "Linksharing",userName:'adminLinksharing' , photo: 121,admin:true,active:true)
-        admin.validate()
-       // admin.save(flush:true)
-         if(admin.save()){
-            log.info("Admin Saved Successfully")
+    void createUsers() {
+        if(User.count()==0) {
+
+            //admin
+            User admin = new User(email: "admin@gmail.com", password: DefaultPassword.PASSWORD, firstName: "admin", lastName: "portal", userName: "adminPortal", /*photo: 121,*/ admin: true, active: true)
+            if(admin.save()){
+                log.info("Admin Saved Successfully")
+            }
+            else {
+                log.error("error: ${admin.errors.getAllErrors()}")
+            }
+
+            //normal
+            User normal = new User(email: "himanshigupta238@gmail.com", password: DefaultPassword.PASSWORD, firstName: "Himanshi", lastName: "Gupta", userName: "HimanshiGupta", /*photo:122,*/ admin: false, active: true)
+            if(normal.save()){
+                log.info("Normal User Saved Successfully")
+            }
+            else {
+                log.error("error: ${normal.errors.getFieldErrors()}")
+            }
+
         }
-        else {
-            log.error("error: ${admin.errors.getAllErrors()}")
-        }
-
-
-        //normal
-        User normal=new User(email: "himanshigupta238@gmail.com",password: "admin@123",firstName: "Himanshi",lastName: "Gupta",userName:'HimanshiGupta',photo: 122,admin:false,active:true)
-        normal.validate()
-        //normal.save(flush:true)
-        if(normal.save()){
-            log.info("Normal User Saved Successfully")
-        }
-        else {
-            log.error("error: ${normal.errors.getFieldErrors()}")
-        }
-
-
-
-
-
-
     }
-    def destroy = {
+
+        def destroy = {
+        }
     }
-}
