e213a84c4d1722b43029e3000884d76f57c9553a
himanshiiiii <himanshi.gupta@tothenew.com>
2018-03-31 14:45:38 +0530

    ques20

--- grails-app/domain/com/ttn/linksharing/User.groovy
+++ grails-app/domain/com/ttn/linksharing/User.groovy
@@ -13,20 +13,26 @@ class User {
     Date dateCreated
     Date lastUpdated
 
+
+    String confirmPassword
     String name
     List<Topic> topics
 
     static transients = ['name']
-    static hasMany = [topics:Topic,subscriptions:Subscription,resources:Resource,readingItems:ReadingItem]
+    static hasMany = [topics:Topic,subscriptions:Subscription,resources:Resource,readingItems:ReadingItem,resourceRating:ResourceRating]
 
     static constraints = {
         email(unique: true,email: true,blank: false,nullable: false)
         userName(unique: true,blank: false,nullable: false)
-        password(blank: false,nullable: false,minSize: 5)
+        password(blank: false,nullable: false,minSize: 5, validator: {password, obj ->
+            def password2 = obj.confirmPassword
+            password == password2 ? true : ['invalid.matchingPasswords']
+        })
         firstName(blank: false,nullable: false)
         lastName(blank: false,nullable: false)
         photo(nullable:true,sqlType:'longBlob')
         admin(nullable:true)
+        confirmPassword(nullable: true,blank: true)
 
     }
 
