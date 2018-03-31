f0d068bb48c43ada7bcdf109ea696d6d694ad5d5
himanshiiiii <himanshi.gupta@tothenew.com>
2018-03-31 22:31:23 +0530

    Q9

--- grails-app/controllers/com/ttn/linksharing/TopicController.groovy
+++ grails-app/controllers/com/ttn/linksharing/TopicController.groovy
@@ -5,16 +5,16 @@ class TopicController {
     def index() {}
 
 
-    def show() {
+    def show(){
+
         User user=User.read(session.user.id)
         //println user.topics
         render(user.topics)
     }
 
-    def delete(Integer id) {
-        Topic topic = Topic.load(id)
+    def delete(Integer id){
+        Topic topic=Topic.load(id)
     }
-
 
     def save(Topic topic,String seriousness){
         if(topic.save()){
