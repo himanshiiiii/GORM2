294009abfe228c4b410b3f1eefc02e7c78d9b3ec
himanshiiiii <himanshi.gupta@tothenew.com>
2018-03-31 21:17:27 +0530

    Q4

--- grails-app/controllers/com/ttn/linksharing/ResourceController.groovy
+++ grails-app/controllers/com/ttn/linksharing/ResourceController.groovy
@@ -1,10 +1,19 @@
 package com.ttn.linksharing
 
+import org.hibernate.ObjectNotFoundException
+
 class ResourceController {
 
     def index() { }
 
     def delete(Integer id){
         Resource resource=Resource.load(id)
+        println resource
     }
-}
+    def handleObjectNotFoundException(ObjectNotFoundException e) {
+
+        render ("no object found")
+    }
+
+    }
+
